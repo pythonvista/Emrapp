064bb3f59ca692d064b3201d84315a9b44afc77c
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 <tsmart247@gmail.com>
2024-12-14 23:44:08 +0000

    initial

---
+++ node_modules/ngx-daterange/interfaces/index.d.ts
@@ -0,0 +1,5 @@
+export { IDateRange } from './IDateRange';
+export { IDateRangePickerOptions } from './IDateRangePickerOptions';
+export { IDefaultDateRanges } from './IDefaultDateRanges';
+export { IDefinedDateRange } from './IDefinedDateRange';
+export { IChangedData } from './IChangedData';
